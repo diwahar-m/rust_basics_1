29c9ca8611ce287812ee5182bc46c8c53e09f74e
diwahar-m <msdiwahar5@gmail.com>
2025-03-13 23:50:34 +0530

    completed 3rd chapter in the book

--- src/main.rs
+++ src/main.rs
@@ -44,25 +44,65 @@ fn main() {
 }
 
 fn second_function(x: u32, y: u32) {
-    println!("This is second function {}, {}", x, y)
+    println!("This is second function {}, {}", x, y);
     let number = five();
-    println!("Returned function {}", number)
+    println!("Returned function {}", number);
 }
 
-fn five() -> i32{
+fn five() -> i32 {
     5 //  here expression is returned
     // placing semicolon here makes it as a statement & it will not return.
 }
 
-
-fn control_flow(){
+fn control_flow() {
     let num = 3;
-    if num >5 { // condition must be boolean
+    if num > 5 {
+        // condition must be boolean
         println!("Num greater than 5")
-    } else if num > 0  {
+    } else if num > 0 {
         println!("Num less than 5")
     } else {
         println!("Num is negative")
+    }
+    if_as_statement()
+}
+
+fn if_as_statement() {
+    let condition = true;
+    // if can be used as a statement
+    let number = if condition { 5 } else { 2 };
+    println!("Number: {}", number);
+    loop_types();
+}
+
+fn loop_types() {
+    // Loop types -> loop, while , for
+
+    // run for infinite times
+    // loop {
+    //     print!("HI !")
+    // }
+
+    let mut number = 2;
+    while number > 0 {
+        println!("{}", number);
+        number = number - 1;
+    }
+    for_loop();
+}
+
+fn for_loop() {
+    for number in (1..4) {
+        println!("For Loop-1, {}", number)
+    }
+    //
+    for number in (1..4).rev() {
+        println!("For Loop-2, {}", number)
+    }
+    //
+    let array = [10, 20, 30];
+    for element in array.iter() {
+        println!("For Loop-3, {}", element)
     }
 }
 // cargo build --release
