ddcaacca3fa0b5e1d473703b1a125fc1230b7c5a
diwahar-m <msdiwahar5@gmail.com>
2025-03-14 14:37:49 +0530

    ownership chapter completed

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::array;
+extern crate rand;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Hello, world1!");
@@ -39,8 +41,10 @@ fn main() {
     let array_one = [1, 2, 3];
     println!("First element in array {}", array_one[0]);
 
-    second_function(5, 6);
-    control_flow()
+    // second_function(5, 6);
+    // control_flow()
+    // guess_num()
+    ownership();
 }
 
 fn second_function(x: u32, y: u32) {
@@ -92,7 +96,7 @@ fn loop_types() {
 }
 
 fn for_loop() {
-    for number in (1..4) {
+    for number in 1..4 {
         println!("For Loop-1, {}", number)
     }
     //
@@ -104,5 +108,105 @@ fn for_loop() {
     for element in array.iter() {
         println!("For Loop-3, {}", element)
     }
+}
+
+fn guess_num() {
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret num is {}", secret_number);
+    println!("Start Guess game");
+
+    loop {
+        println!("Print the guess number: ");
+
+        let mut guess = String::new();
+
+        std::io::stdin()
+            .read_line(&mut guess) // mutable reference
+            .expect("Failed the guess!");
+        // let guess: u32 = guess.trim().parse().expect("Please type num"); // will break if there is error;
+        let guess: u32 = match guess.trim().parse() {
+            // pattern matching
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("The guess num is {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
+
+fn ownership() {
+    let s1 = String::from("Hello");
+    // let s2 = s1 //  here ownership of s1 is went to s2.
+    // returning values also transfer ownership.
+    let s2 = s1.clone();
+    println!("S1: {}", s1);
+
+    // borrowing
+    let val_1 = 33;
+    let returned_value = transfering_ownership(val_1);
+    println!("The value of Val_1 is: {}", returned_value);
+
+    // referencing - immutable
+    let val_2 = String::from("hello");
+    let returned_value_2 = referencing(&val_2);
+    println!("The value of Val_2 is: {}", returned_value_2);
+
+    // data race (mutating value)- can be avoided by using scope;
+    let mut val_3 = String::from("HI");
+    {
+        let mut_val_3 = &mut val_3;
+    }
+    let mut_val_4 = &mut val_3;
+
+    // dangle reference
+    // dangle();
+    // slice type
+    let s = String::from("Hello World!");
+    slice_type(&s);
+
+    // string slice
+    let u = String::from("Hi");
+    let u_len = u.len();
+    let u_2 = &s[..u_len];
+    let u_3 = &s[3..];
+    let u_all = &s[..];
+
+    // array slice
+    let arr_1 = [1, 2, 3, 4, 5];
+    let slice = &arr_1[0..4];
+    println!("Array slice: {}", slice[0]);
+}
+
+fn transfering_ownership(val: u32) -> u32 {
+    val
+}
+fn referencing(val: &String) -> &String {
+    val
+}
+
+fn dangle() -> String {
+    let s = String::from("ss");
+    s // returning ownership here to avoid dangle reference here.
+} // scope of s is dropped here.
+
+fn slice_type(s: &String) -> &str {
+    let bytes = s.as_bytes(); // converting to byte slice
+    for (i, &item) in bytes.iter().enumerate() {
+        // converting to tuple to get index
+        if item == b' ' {
+            // checks for a byte of space.
+            return &s[..i];
+        }
+    }
+    return &s[..];
 }
 // cargo build --release
